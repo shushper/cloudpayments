86f63e9bab031edf209607a416f640c9a98b375c
Renat Shakhmatov <shushper@gmail.com>
2020-10-19 15:29:41 +0500

    Left only one CheckoutScreen.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
 
-import 'package:cloudpayments_example/screens/card_screen.dart';
-import 'package:cloudpayments_example/screens/main_screen.dart';
+import 'package:cloudpayments/cloudpayments.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
+import 'common/custom_button.dart';
+import 'constants.dart';
+import 'network/api.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +19,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -28,15 +32,271 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => MainScreen(),
-        '/card': (context) => CardScreen(),
-      },
+      home: CheckoutScreen(),
     );
   }
 }
 
+class CheckoutScreen extends StatefulWidget {
+  @override
+  _CheckoutScreenState createState() => _CheckoutScreenState();
+}
 
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final cardHolderController = TextEditingController();
+  final cardNumberMaskFormatter = MaskTextInputFormatter(mask: '#### #### #### ####');
+  final expireDateFormatter = MaskTextInputFormatter(mask: '##/##');
+  final cvcDateFormatter = MaskTextInputFormatter(mask: '###');
+  final api = Api();
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
 
+  bool _isInvalidAsyncCardHolder = false;
+  bool _isInvalidAsyncCardNumber = false;
+  bool _isInvalidAsyncExpireDate = false;
+  bool _isInvalidAsyncCvcCode = false;
 
+  String _validateCardHolder(String cardHolder) {
+    if (_isInvalidAsyncCardHolder) {
+      _isInvalidAsyncCardHolder = false;
+      return 'Card holder can\'t be blank';
+    }
+    return null;
+  }
+
+  String _validateCardNumber(String cardNumber) {
+    if (_isInvalidAsyncCardNumber) {
+      _isInvalidAsyncCardNumber = false;
+      return 'Invalid card number';
+    }
+    return null;
+  }
+
+  String _validateExpireDate(String expireDate) {
+    if (_isInvalidAsyncExpireDate) {
+      _isInvalidAsyncExpireDate = false;
+      return 'Date invalid or expired';
+    }
+    return null;
+  }
+
+  String _validateCvv(String cvc) {
+    if (_isInvalidAsyncCvcCode) {
+      _isInvalidAsyncCvcCode = false;
+      return 'Incorrect cvv code';
+    }
+    return null;
+  }
+
+  void _onPayClick() async {
+    print('_onPayClick');
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+
+      FocusScope.of(context).unfocus();
+
+      final cardHolder = cardHolderController.text;
+      final isCardHolderValid = cardHolder.isNotEmpty;
+
+      final cardNumber = cardNumberMaskFormatter.getUnmaskedText();
+      final isValidCardNumber = await Cloudpayments.isValidNumber(cardNumber);
+
+      final expireDate = expireDateFormatter.getMaskedText();
+      final isValidExpireDate = await Cloudpayments.isValidExpireDate(expireDate);
+
+      final cvcCode = cvcDateFormatter.getUnmaskedText();
+      final isValidCvcCode = cvcCode.length == 3;
+
+      if (!isCardHolderValid) {
+        setState(() {
+          _isInvalidAsyncCardHolder = true;
+        });
+      } else if (!isValidCardNumber) {
+        setState(() {
+          _isInvalidAsyncCardNumber = true;
+        });
+      } else if (!isValidExpireDate) {
+        setState(() {
+          _isInvalidAsyncExpireDate = true;
+        });
+      } else if (!isValidCvcCode) {
+        setState(() {
+          _isInvalidAsyncCvcCode = true;
+        });
+      } else {
+        final cryptogram = await Cloudpayments.cardCryptogram(
+          cardNumber,
+          expireDate,
+          cvcCode,
+          Constants.MERCHANT_PUBLIC_ID,
+        );
+
+        print('Cryptogram = ${cryptogram.cryptogram}, Error = ${cryptogram.error}');
+
+        if (cryptogram.cryptogram != null) {
+          _auth(cryptogram.cryptogram, cardHolder, 1);
+        }
+      }
+    }
+  }
+
+  void _auth(String cryptogram, String cardHolder, int amount) async {
+    final transaction = await api.auth(cryptogram, cardHolder, amount);
+    if (transaction.paReq != null && transaction.ascUrl != null) {
+    } else {
+      _scaffoldKey.currentState
+          .showSnackBar(SnackBar(content: Text(transaction.cardHolderMessage)));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _formKey.currentState?.validate();
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Checkout'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Total to be paid: 2 RUB.',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  controller: cardHolderController,
+                  keyboardType: TextInputType.name,
+                  textCapitalization: TextCapitalization.words,
+                  textInputAction: TextInputAction.next,
+                  validator: _validateCardHolder,
+                  decoration: InputDecoration(
+                    labelText: 'Card holder',
+                    labelStyle: TextStyle(color: Colors.grey),
+                    border: UnderlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [cardNumberMaskFormatter],
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    labelText: 'Card number',
+                    labelStyle: TextStyle(color: Colors.grey),
+                    border: UnderlineInputBorder(),
+                  ),
+                  validator: _validateCardNumber,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        textInputAction: TextInputAction.next,
+                        inputFormatters: [expireDateFormatter],
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Expire Date',
+                          labelStyle: TextStyle(color: Colors.grey),
+                          border: UnderlineInputBorder(),
+                        ),
+                        validator: _validateExpireDate,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                          textInputAction: TextInputAction.done,
+                          inputFormatters: [cvcDateFormatter],
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            labelText: 'CVC',
+                            labelStyle: TextStyle(color: Colors.grey),
+                            border: UnderlineInputBorder(),
+                          ),
+                          validator: _validateCvv),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                CustomButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Pay with card',
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Icon(Icons.credit_card),
+                    ],
+                  ),
+                  onPressed: _onPayClick,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text('or'),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                if (Platform.isAndroid)
+                  CustomButton(
+                    backgroundColor: Colors.black,
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pay with',
+                          textAlign: TextAlign.center,
+                        ),
+                        SvgPicture.asset(
+                          'assets/images/google_pay.svg',
+                          height: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                if (Platform.isIOS)
+                  CustomButton(
+                    backgroundColor: Colors.black,
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pay with Apple',
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
