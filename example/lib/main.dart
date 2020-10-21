b4a162d51bd0483bd139b56afc32bb4582027882
Renat Shakhmatov <shushper@gmail.com>
2020-10-21 17:38:55 +0500

    Hide debug mode banner.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,6 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: CheckoutScreen(),
     );
   }
